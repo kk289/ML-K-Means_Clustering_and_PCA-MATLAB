57804a7297347fde3a53231080d883d3f7dfcc7b
kk289 <kevilkhadka@gmail.com>
2020-06-22 00:54:44 -0700

    projecting the data onto the principle component

--- projectData.m
+++ projectData.m
@@ -4,10 +4,7 @@ function Z = projectData(X, U, K)
 %   Z = projectData(X, U, K) computes the projection of 
 %   the normalized inputs X into the reduced dimensional space spanned by
 %   the first K columns of U. It returns the projected examples in Z.
-%
 
-% You need to return the following variables correctly.
-Z = zeros(size(X, 1), K);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the projection of the data using only the top K 
@@ -18,9 +15,6 @@ Z = zeros(size(X, 1), K);
 %                    projection_k = x' * U(:, k);
 %
 
-
-
-
-% =============================================================
+Z = X * U(:,1:K);
 
 end
